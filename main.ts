cfd559e43e608b07f60186f0c1d530b3c78db7b5
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-07 08:42:54 -0700

    Starting sprite math helpers

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 
+namespace spritemath {
+    //% block="distance between $a and $b"
+    //% a.shadow=variables_get
+    //% a.defl=mySpriteA
+    //% b.shadow=variables_get
+    //% b.defl=mySpriteB
+    export function distance(a: Sprite, b: Sprite): number {
+        return Math.sqrt(distanceSqr(a, b))
+    }
+
+    export function distanceSqr(a: Sprite, b: Sprite): number {
+        const dx = a.x - b.x
+        const dy = a.y - b.y
+        return dx ^ 2 + dy ^ 2
+    }
+
+    
+}
